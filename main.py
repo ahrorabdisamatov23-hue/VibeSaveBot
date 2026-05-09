cc314753859c79bef932ab18f75228ce8d5045e7
ahrorabdisamatov23-hue <ahrorabdisamatov23@gmail.com>
2026-05-09 17:30:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -357,4 +357,18 @@ def added(message):
 
 
 print("BOT ISHLADI...")
+
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot ishlayapti"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run).start()
 bot.infinity_polling()
